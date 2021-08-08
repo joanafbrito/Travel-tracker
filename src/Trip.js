49acb550bee5ebdf51d9ceefd05ef58b53d48057
Joana Brito <joanafiorentini@hotmail.com>
2021-08-07 17:42:00 -0700

    Create class for trip

---
+++ src/Trip.js
@@ -0,0 +1,17 @@
+class Trip {
+    constructor(tripData) {
+      this.id = tripData.id;
+      this.userID = tripData.userID;
+      this.destinationID = tripData.destinationID;
+      this.travelers = tripData.travelers;
+      this.date = tripData.date;
+      this.duration = tripData.duration;
+      this.status = tripData.status;
+      this.suggestedActivities = tripData.suggestedActivities;
+    }
+    //  trip cost?
+
+  }
+  
+  export default Trip;
+  
